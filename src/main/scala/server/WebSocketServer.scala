2a9b3c0d3b6b13848ef03e33ddf9705c035f5360
TheBugYouCantFix <alex.gorbanev05@gmail.com>
2025-04-20 16:10:59 +0300

    feat: define websocket server template

--- src/main/scala/server/WebSocketServer.scala
+++ src/main/scala/server/WebSocketServer.scala
@@ -3,16 +3,33 @@ package server
 import zio.http.*
 import zio.*
 
-val socket = Handler.webSocket { channel =>
-  channel.receiveAll {
-    case ChannelEvent.Read(WebSocketFrame.Text("foo")) =>
-      channel.send(ChannelEvent.Read(WebSocketFrame.text("bar")))
-    case _ =>
-      ZIO.unit
+import io.circe.parser.*
+import io.circe.Json
+
+import protocol.*
+
+
+object WebSocketServer extends ZIOAppDefault {
+  private val socketApp = Handler.webSocket { channel =>
+    channel.receiveAll {
+      case ChannelEvent.Read(WebSocketFrame.Text(jsonString)) =>
+        decode[Operation](jsonString) match {
+          case op: AddString => // call handler 
+          case op: RemoveNCharacters => // call handler
+          case op: Undo => // call handler
+          case op: Redo => // call handler
+        }
+
+      case _ =>
+        ZIO.unit
+    }
   }
+
+  private val routes = Routes(
+    Method.GET / "updates" -> handler(socketApp.toResponse)
+  )
+
+  override val run: ZIO[Any, Throwable, Nothing] = Server.serve(routes).provide(Server.default)
 }
 
-// a preliminary template of a route that asks a client for updates
-val http = Routes(
-  Method.GET / "updates" -> handler(socket.toResponse)
-)
+
