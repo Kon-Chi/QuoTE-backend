964ccc7036aa7fe09ca31815837933c18cf21ac0
TheBugYouCantFix <alex.gorbanev05@gmail.com>
2025-04-25 23:56:34 +0300

    feat: create clients list ref

--- src/main/scala/server/WebSocketServer.scala
+++ src/main/scala/server/WebSocketServer.scala
@@ -3,19 +3,26 @@ package server
 import zio.http.*
 import zio.*
 
+import io.circe.*
+import io.circe.syntax.*
+
 import java.util.UUID
 
 import io.circe.parser.*
 
 import models.*
+import quote.ot.*
 
 type OpError = String
+type RefClients = Ref[List[(UUID, WebSocketChannel)]]
 
 object WebSocketServer extends ZIOAppDefault {
   private final val queueSize = 1000
-  private def socketApp(queue: Queue[UserOperations]) = Handler.webSocket { channel =>
+  private def socketApp(queue: Queue[UserOperations], clients: RefClients) = Handler.webSocket { channel =>
     for {
-      clientId <- Random.nextUUID.map(_.toString)
+      clientId <- Random.nextUUID
+      _ <- clients.update((clientId, channel) :: _)
+
       _ <- channel.receiveAll {
         case ChannelEvent.Read(WebSocketFrame.Text(jsonString)) =>
           for {
@@ -34,16 +41,17 @@ object WebSocketServer extends ZIOAppDefault {
 
   override val run: ZIO[Any, Throwable, Nothing] = for {
     queue: Queue[UserOperations] <- Queue.bounded(queueSize)
-    server <- Server.serve(routes(queue)).provide(Server.default)
+    clients: RefClients <- Ref.make(List.empty)
+    server <- Server.serve(routes(queue, clients)).provide(Server.default)
   } yield server
 }
 
 def applyOp(op: Operation)(text: String): Either[OpError, String] =
   op match
     case Insert(index, str) => 
-      if index >= text.size || index < 0 then Left("Failed to apply Insert operation")
+      if index >= text.length || index < 0 then Left("Failed to apply Insert operation")
       else Right(text.take(index) + str + text.drop(index))
     case Delete(index, len) => 
-      if len > text.size || index < 0 || index >= text.size then Left("Failed to apply Delete operation")
+      if len > text.length || index < 0 || index >= text.length then Left("Failed to apply Delete operation")
       else Right(text.take(index) + text.drop(index + len))
 
