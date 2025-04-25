c88378d7853f3004179bf6a04eeb6c890b4aedfa
Leropsis <v.ksenofontov@innopolis.university>
2025-04-25 22:05:43 +0300

    Add applyOp function to WebSocketServer

--- src/main/scala/server/WebSocketServer.scala
+++ src/main/scala/server/WebSocketServer.scala
@@ -7,6 +7,8 @@ import io.circe.parser.*
 
 import quote.ot.*
 
+type OpError = String
+
 object WebSocketServer extends ZIOAppDefault {
   private final val queueSize = 1000
   private val socketApp = Handler.webSocket { channel =>
@@ -31,4 +33,13 @@ object WebSocketServer extends ZIOAppDefault {
   override val run: ZIO[Any, Throwable, Nothing] = Server.serve(routes).provide(Server.default)
 }
 
+def applyOp(op: Operation)(text: String): Either[OpError, String] =
+  op match
+    case Insert(index, str) => 
+      if index >= text.size || index < 0 then Left("Failed to apply Insert operation")
+      else Right(text.take(index) + str + text.drop(index))
+    case Delete(index, len) => 
+      if len > text.size || index < 0 || index >= text.size then Left("Failed to apply Delete operation")
+      else Right(text.take(index) + text.drop(index + len))
+    case _ => Left("Unexpected operaion appeared")
 
