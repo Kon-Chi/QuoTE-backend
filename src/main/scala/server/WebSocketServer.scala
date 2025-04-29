5f0ebc54f4891fb1b4ec91734ed3dfccd1182b21
TheBugYouCantFix <alex.gorbanev05@gmail.com>
2025-04-29 13:04:06 +0300

    fix: finally it compiles

--- src/main/scala/server/WebSocketServer.scala
+++ src/main/scala/server/WebSocketServer.scala
@@ -12,25 +12,14 @@ import java.util.UUID
 
 import models.*
 import quote.ot.*
-//import server.ProtobufCodecSupplier
+
 import zio.redis.*
 import zio.schema.*
 import zio.schema.codec.*
 
 
-object StringCodecSupplier extends CodecSupplier {
-  def get[A: Schema]: BinaryCodec[A] = new BinaryCodec[A] {
-    def encode(value: A): Chunk[Byte] = Chunk.fromArray(value.toString.getBytes("UTF-8"))
-
-    def decode(chunk: Chunk[Byte]): Either[DecodeError, A] = {
-      try {
-        val str =new String(chunk.toArray, "UTF-8")
-        Right(str.asInstanceOf[A])
-      } catch {
-        case e: Exception => Left(DecodeError.ReadError(zio.Cause.empty, e.getMessage))
-      }
-    }
-  }
+object ProtobufCodecSupplier extends CodecSupplier {
+  def get[A: Schema]: BinaryCodec[A] = ProtobufCodec.protobufCodec
 }
 
 
@@ -108,34 +97,32 @@ object WebSocketServer extends ZIOAppDefault:
       _ <- redis.set("doc", doc.toString)
     yield ()
 
-  override val run: ZIO[ZIOAppArgs & Scope, Nothing, ExitCode] = for
+  override val run = for
     queue <- Queue.bounded[ClientOperations](queueSize)
     clients <- Ref.make(List.empty[(UUID, WebSocketChannel)])
-    serverState <- Ref.make[ServerState](0, " ", Nil) // I think we need init document fetching from db or smth
+    serverState <- Ref.make[ServerState](0, " ", Nil)
 
     queueLayer = ZLayer.succeed(queue)
     clientsLayer = ZLayer.succeed(clients)
     serverStateLayer = ZLayer.succeed(serverState)
-    codecLayer = ZLayer.succeed[CodecSupplier](StringCodecSupplier)
-    redisLayer = Redis.local/pr
+    codecLayer = ZLayer.succeed[CodecSupplier](ProtobufCodecSupplier)
+    redisLayer = Redis.local
 
-    appLayer = serverStateLayer ++ queueLayer ++ clientsLayer ++ codecLayer ++ redisLayer
-    _ <- opProcess
-      .provideLayer(appLayer)
-      .forever
-      .fork
+    appLayer =  codecLayer ++ serverStateLayer ++ queueLayer ++ clientsLayer ++ redisLayer
+    _ <- opProcess.forever.fork.provide(
+      codecLayer,
+      redisLayer,
+      serverStateLayer,
+      queueLayer,
+      clientsLayer
+    )
+
     exitCode <- Server
       .serve(routes(queue, clients))
-      .provide(Server.defaultWith(_.binding("127.0.0.1", 8000)))
+      .provide(Server.defaultWith(_.binding("127.0.0.1", 8080)))
       .exitCode
   yield exitCode
 
 
-def applyOp(op: Operation, text: Document): Either[OpError, Document] = op match
-  case Insert(index, str) => if index >= text.length || index < 0
-  then Left("Failed to apply Insert operation")
-  else Right(text.take(index) + str + text.drop(index))
-  case Delete(index, len) => if len > text.length || index < 0 || index >= text.length
-  then Left("Failed to apply Delete operation")
-  else Right(text.take(index) + text.drop(index + len))
+def applyOp(op: Operation, text: Document): Either[OpError, Document] = ???
 
